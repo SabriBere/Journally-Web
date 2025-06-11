442e3c03e16fa276d9a098235ae4d97a626e4d76
SabriBere <sabrinab786@gmail.com>
2025-06-11 20:12:59 -0300

    eliminando estilos de prueba para sass

--- src/components/Buttons/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./button.module.scss";
-
-const Button = () => {
-    return <button className={styles.buttonFilter}>{"Filtrar"}</button>;
-};
-
-export default Button;
