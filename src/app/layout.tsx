2463f29cb1847b4c22286cd3cd07ad4bf50825f5
SabriBere <sabrinab786@gmail.com>
2025-06-14 13:44:10 -0300

    definición de fuentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
 import type { Metadata } from "next";
+import { Caveat, Inter } from "next/font/google";
 import Providers from "./providers";
 import "./globals.scss";
+
+export const caveat = Caveat({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-caveat",
+});
+
+export const inter = Inter({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
     title: "Journally App",
@@ -12,8 +25,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body>
+        <html lang="en" className={`${caveat.variable} ${inter.variable}`}>
+            <body className="font-body">
                 <Providers>{children}</Providers>
             </body>
         </html>
