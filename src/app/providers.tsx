d032cc175465a51d05eca5fe4f6c3e25f62518c6
SabriBere <sabrinab786@gmail.com>
2025-06-14 12:43:54 -0300

    ajuste es el formato y fondo de la app

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-    const queryClient = new QueryClient()
-  return (
-    <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools buttonPosition="bottom-right" />
-    </QueryClientProvider>
-  )
-}
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools buttonPosition="bottom-right" />
+        </QueryClientProvider>
+    );
+};
 
-export default Providers
+export default Providers;
