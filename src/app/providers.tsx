1e3f9d1befd86e502a9cb5fa7d11f203d052194a
SabriBere <sabrinab786@gmail.com>
2025-06-14 12:38:30 -0300

    agregando tan stack query

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  )
+}
+
+export default Providers
