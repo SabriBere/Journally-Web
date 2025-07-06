e41b76b4eb9ef8d9172d7dd2d32160aac89bbc72
SabriBere <sabrinab786@gmail.com>
2025-07-06 18:02:13 -0300

    usando variables dentro de los iconos

--- src/styles/icons/CaretDown.tsx
+++ src/styles/icons/CaretDown.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import { ColorProps } from "./Icons.types";
 
 const CaretDown = ({ color, width, height }: ColorProps) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 256 256"
-    >
+    <svg fill={color} width={width} height={height} viewBox="0 0 256 256">
         <path d="m213.66 101.66-80 80a8 8 0 0 1-11.32 0l-80-80a8 8 0 0 1 11.32-11.32L128 164.69l74.34-74.35a8 8 0 0 1 11.32 11.32"></path>
     </svg>
 );
--- src/styles/icons/Close.tsx
+++ src/styles/icons/Close.tsx
@@ -3,12 +3,7 @@ import { ColorProps } from "./Icons.types";
 
 function Close({ color, width, height }: ColorProps) {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={32}
-            height={32}
-            viewBox="0 0 256 256"
-        >
+        <svg fill={color} width={width} height={height} viewBox="0 0 256 256">
             <path d="M205.66 194.34a8 8 0 01-11.32 11.32L128 139.31l-66.34 66.35a8 8 0 01-11.32-11.32L116.69 128 50.34 61.66a8 8 0 0111.32-11.32L128 116.69l66.34-66.35a8 8 0 0111.32 11.32L139.31 128z" />
         </svg>
     );
