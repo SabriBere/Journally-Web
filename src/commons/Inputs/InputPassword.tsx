6159995d508a1e58d4f7f0b3a1b9d64558d6924d
SabriBere <sabrinab786@gmail.com>
2025-07-11 23:06:23 -0300

    inicio de maquteado input password

---
+++ src/commons/Inputs/InputPassword.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./inputPassword.module.scss";
+
+const InputPassword = () => {
+    const [inputPassword, setInputPass] = useState<string>("");
+    // const [isValidPass, setIsValidPass] = useState<boolean>(false);
+    // const passRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+    const handleChangePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setInputPass(value);
+        // setIsValidPass(passRegex.test(value));
+    };
+
+    return (
+        <div>
+            <input
+                type="password"
+                placeholder="Contraseña"
+                onChange={handleChangePass}
+                value={inputPassword}
+            />
+        </div>
+    );
+};
+
+export default InputPassword;
