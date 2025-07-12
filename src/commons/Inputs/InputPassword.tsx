872149ed20cc16fe35ebec3dc3e42ea1b2682523
SabriBere <sabrinab786@gmail.com>
2025-07-12 09:40:02 -0300

    maquetado inicial de formulario de login

--- src/commons/Inputs/InputPassword.tsx
+++ src/commons/Inputs/InputPassword.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import styles from "./inputPassword.module.scss";
 
 const InputPassword = () => {
+    //Mover al hook de password
     const [inputPassword, setInputPass] = useState<string>("");
     // const [isValidPass, setIsValidPass] = useState<boolean>(false);
     // const passRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
