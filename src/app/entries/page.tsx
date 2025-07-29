9a4dc8deeb3383a773c26181c96fb1c57372e383
Sabrina Demetrio <sabrinab786@gmail.com>
2025-07-29 10:56:24 -0300

    consulta a un end point usando solo token (sin refresh)

--- src/app/entries/page.tsx
+++ src/app/entries/page.tsx
@@ -1,5 +1,5 @@
 "use client"; //hasta que modularice en componentes más pequeños
-import React from "react";
+import React, { Fragment } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getAllPost } from "@/services/post.service";
 import styles from "./entries.module.scss";
@@ -18,6 +18,7 @@ const Entries = () => {
         queryKey: ["dossiersInModal"],
         queryFn: ({ pageParam = 1 }) => getAllPost({ page: pageParam }),
         getNextPageParam: (lastPage: any, pages: any) => {
+            //revisar si recibe toda la data necesaria de la API
             if (pages?.length - 1 < lastPage?.limit) {
                 return pages.length;
             }
@@ -26,12 +27,22 @@ const Entries = () => {
         initialPageParam: 1,
     });
 
-    console.log(data?.pages[0]?.userPost)
+    console.log(data?.pages[0]?.userPost);
 
     return (
         <div className={styles.containerEntries}>
             <h1>Entries</h1>
-            {/* <h1>{`${data?.userPost}`}</h1> */}
+            {isSuccess && (
+                <ul>
+                    {data?.pages?.map((page: any, pageIndex: number) => (
+                        <Fragment key={pageIndex}>
+                            {page?.userPost.map((onePost: any) => (
+                                <li key={onePost?.post_id}>{onePost?.title}</li>
+                            ))}
+                        </Fragment>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
