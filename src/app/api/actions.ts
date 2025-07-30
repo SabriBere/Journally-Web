7873fa59ca9cc5eba42a645d5fde2f1180f55cba
SabriBere <sabrinab786@gmail.com>
2025-07-30 12:33:53 -0300

    refactor en credentials, función userLogin

--- src/app/api/actions.ts
+++ src/app/api/actions.ts
@@ -1,6 +1,38 @@
+"use server";
 //consulta a end point login y end point refresh
-
 import axios from "axios";
+
+export async function userLoging({
+    email,
+    password,
+}: {
+    email: string;
+    password: string;
+}) {
+    const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/login`,
+        {
+            email: email,
+            password: password,
+        }
+    );
+
+    if (response.status === 201 && response.data?.data) {
+        const userData = response?.data?.data;
+        const accessToken = response.headers["x-access-token"];
+        const refreshToken = response.headers["x-refresh-token"];
+
+        // console.log(userData?.refresh, "llega el refresh en respuesta?");
+        return {
+            id: userData?.userId,
+            name: userData?.userName,
+            email: userData?.user,
+            accessToken,
+            refreshToken,
+        };
+    }
+    return null;
+}
 
 async function refreshToken(token: string) {
     try {
