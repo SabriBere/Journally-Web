f28935715ce55bfa4063243f829b7021a7f68e3c
SabriBere <sabrinab786@gmail.com>
2025-08-04 12:36:05 -0300

    maquetado del titulo de las secciones

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import TitleSection from "@/commons/Title/TitleSection";
 import styles from "./home.module.scss";
 
 const Home = () => {
     return (
         <div className={styles.containerHome}>
-            <h1>Home</h1>
+            <TitleSection title={"Posteos"} />
         </div>
     );
 };
