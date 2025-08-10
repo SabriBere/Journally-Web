f3910c8d23d779981d459b9424f4e7c26bc32c9e
SabriBere <sabrinab786@gmail.com>
2025-08-10 02:34:03 -0300

    inicio de maquetado de Empty State de error

---
+++ src/commons/EmptyStates/Error.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import styles from "./error.module.scss";
+
+const Error = () => {
+    return (
+        <div className={styles.containerEmptyState}>
+            <h2>Ooop... Surgió un problema</h2>
+        </div>
+    );
+};
+
+export default Error;
