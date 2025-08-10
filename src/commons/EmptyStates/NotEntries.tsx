abcee65a70c20c1fcd382bd63b8ac855c5496887
SabriBere <sabrinab786@gmail.com>
2025-08-10 00:55:39 -0300

    maquetado de empty state para crear una nueva entrada

--- src/commons/EmptyStates/NotEntries.tsx
+++ src/commons/EmptyStates/NotEntries.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import Plus from "@/styles/icons/Plus";
+import Plus from "@/styles/icons/Plus";
 import styles from "./notEntries.module.scss";
 
 const NotEntries = () => {
     return (
         <div className={styles.containerEmptyState}>
-            <h3>Crear una nueva entrada</h3>
+            <h2>Crear una nueva entrada</h2>
             <button>
-                {/* <Plus color="white" width="24" height="24" /> */}+
+                <Plus color="white" width="24" height="24" />
             </button>
         </div>
     );
