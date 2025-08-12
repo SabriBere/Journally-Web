46d76e8fe52db07d28a96ca4566a2012b1776f27
SabriBere <sabrinab786@gmail.com>
2025-08-11 22:00:10 -0300

    iniciando maquetado de tabs

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import TitleSection from "@/commons/Title/TitleSection";
 import ListCollections from "./components/ListCollections";
 import styles from "./home.module.scss";
+import Tabs from "@/commons/Tabs/Tabs";
 
 const Home = () => {
     return (
         <div className={styles.containerHome}>
             <TitleSection title={"Inicio"} />
             {/* Agregar componente de pestañas */}
-            <ListCollections />
+            <Tabs />
+            {/* Agregar componente de input search con funcionalidad */}
+            {/* <ListCollections /> */}
         </div>
     );
 };
