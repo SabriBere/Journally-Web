58823890f81494c6ce246718cd06684babf30829
SabriBere <sabrinab786@gmail.com>
2025-08-11 22:54:30 -0300

    acomodando componentes en vista inicio

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import TitleSection from "@/commons/Title/TitleSection";
-import ListCollections from "./components/ListCollections";
-import styles from "./home.module.scss";
 import Tabs from "@/commons/Tabs/Tabs";
+import ListCollections from "./components/ListCollections";
 import ListPost from "./components/ListPost";
+import styles from "./home.module.scss";
 
 const Home = () => {
     return (
         <div className={styles.containerHome}>
-            <TitleSection title={"Inicio"} />
-            <div>
+            {/* <TitleSection title={"Inicio"} /> */}
+            <div className={styles.containerHeader}>
                 {/* Agregar componente de pestañas */}
                 <Tabs />
                 {/* Agregar componente de input search con funcionalidad */}
