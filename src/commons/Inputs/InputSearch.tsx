4cb2cb1e6db207be12ac80f80eb0259049d559f8
SabriBere <sabrinab786@gmail.com>
2025-08-12 22:11:19 -0300

    inicio de maquedato de input search reutilizable para pestañas

---
+++ src/commons/Inputs/InputSearch.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const InputSearch = () => {
+    //el nombre del label y placeholder cambien según el tab seleccionado
+    return (
+        <div>
+            <label>Buscar</label>
+            <input placeholder="Buscar colección"></input>
+        </div>
+    );
+};
+
+export default InputSearch;
