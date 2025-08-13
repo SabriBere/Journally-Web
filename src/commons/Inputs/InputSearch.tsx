b5b42d88893327fcf3681dc4c1892042ce79e0fa
SabriBere <sabrinab786@gmail.com>
2025-08-12 22:18:49 -0300

    inicio de maquedato de input search reutilizable para pestañas

--- src/commons/Inputs/InputSearch.tsx
+++ src/commons/Inputs/InputSearch.tsx
@@ -1,11 +1,18 @@
 "use client";
 import React from "react";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
+import styles from "./inputSearch.module.scss";
 
 const InputSearch = () => {
     //el nombre del label y placeholder cambien según el tab seleccionado
+    const tabs = useSelector((state: RootState) => state.tabs.tabs);
+
     return (
-        <div>
-            <label>Buscar</label>
+        <div className={styles.containerInputSearch}>
+            <label>
+                {tabs === "collections" ? "Buscar colección" : "Buscar posteo"}
+            </label>
             <input placeholder="Buscar colección"></input>
         </div>
     );
