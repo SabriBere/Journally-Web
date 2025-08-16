4b6d1c14cc1e65fec022b58e4fbe4a5a25536239
SabriBere <sabrinab786@gmail.com>
2025-08-16 12:13:52 -0300

    inicio de captura de valores con estados globales de readux

--- src/commons/Inputs/InputSearch.tsx
+++ src/commons/Inputs/InputSearch.tsx
@@ -1,15 +1,22 @@
 "use client";
 import React from "react";
 import { RootState } from "@/store/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./inputSearch.module.scss";
 import Search from "@/styles/icons/Search";
 
 const InputSearch = () => {
+    const dispatch = useDispatch();
     //el nombre del label y placeholder cambien según el tab seleccionado
     const tabs = useSelector((state: RootState) => state.tabs.tabs);
 
     //capturar el valor del input y enviar a los listados para filtrar
+    const searchTextPost = useSelector(
+        (state: RootState) => state.user.searchTextPost
+    );
+    const searchTextCollection = useSelector(
+        (state: RootState) => state.user.searchTextCollection
+    );
 
     return (
         <div className={styles.containerInputSearch}>
