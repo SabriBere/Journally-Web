2d198380b1f4ed382784726255d365245d84aaea
SabriBere <sabrinab786@gmail.com>
2025-08-16 12:04:24 -0300

    estilización de input search

--- src/styles/icons/Search.tsx
+++ src/styles/icons/Search.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ColorProps } from "./Icons.types";
 
-function SvgComponent({ color, width, height }: ColorProps) {
+function Search({ color, width, height }: ColorProps) {
     return (
         <svg fill={color} width={width} height={height} viewBox="0 0 256 256">
             <path d="M229.66 218.34l-50.07-50.06a88.11 88.11 0 10-11.31 11.31l50.06 50.07a8 8 0 0011.32-11.32zM40 112a72 72 0 1172 72 72.08 72.08 0 01-72-72z" />
@@ -9,4 +9,4 @@ function SvgComponent({ color, width, height }: ColorProps) {
     );
 }
 
-export default SvgComponent;
+export default Search;
