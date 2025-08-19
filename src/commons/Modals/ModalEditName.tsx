02f1109288b59609001cd71ee6935ef29b6fb348
SabriBere <sabrinab786@gmail.com>
2025-08-19 20:49:33 -0300

    apertura/cierre del modal editar

--- src/commons/Modals/ModalEditName.tsx
+++ src/commons/Modals/ModalEditName.tsx
@@ -1,8 +1,35 @@
 "use client";
 import React from "react";
+import styles from "./modalEditName.module.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
-const ModalEditName = () => {
-    return <div>ModalEditName</div>;
+interface ModalProps {
+    id: number;
+    isOpen: boolean;
+    setClose: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+//Reutilizar para titulo de colecciones/posteos
+const ModalEditName = ({ id, isOpen, setClose }: ModalProps) => {
+    return (
+        <div className={styles.containerModalName}>
+            <div className={styles.containerInputs}>
+                {/* Mostrar en el placeholder el nombre */}
+                <input
+                    className={styles.inputName}
+                    placeholder="Ingrese nuevo nombre"
+                    type="text"
+                />
+                <button
+                    className={styles.buttonEdit}
+                    onClick={() => setClose(false)}
+                >
+                    Editar
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ModalEditName;
