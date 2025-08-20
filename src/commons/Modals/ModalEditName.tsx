01046a041884828ed205fa5c088ed610aa2237a2
SabriBere <sabrinab786@gmail.com>
2025-08-19 22:28:23 -0300

    estilos del input edit s/la card

--- src/commons/Modals/ModalEditName.tsx
+++ src/commons/Modals/ModalEditName.tsx
@@ -1,33 +1,43 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import Close from "@/styles/icons/Close";
+import Check from "@/styles/icons/Check";
 import styles from "./modalEditName.module.scss";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
 
 interface ModalProps {
     id: number;
     isOpen: boolean;
     setClose: React.Dispatch<React.SetStateAction<boolean>>;
+    color: string;
 }
 
 //Reutilizar para titulo de colecciones/posteos
-const ModalEditName = ({ id, isOpen, setClose }: ModalProps) => {
+const ModalEditName = ({ id, isOpen, setClose, color }: ModalProps) => {
+    const [newName, setNewName] = useState<string>("");
+    //Mutación con react query
+    const handlerEdit = async (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setClose((prev: boolean) => !prev);
+    };
+
     return (
-        <div className={styles.containerModalName}>
-            <div className={styles.containerInputs}>
-                {/* Mostrar en el placeholder el nombre */}
-                <input
-                    className={styles.inputName}
-                    placeholder="Ingrese nuevo nombre"
-                    type="text"
-                />
-                <button
-                    className={styles.buttonEdit}
-                    onClick={() => setClose(false)}
-                >
-                    Editar
-                </button>
-            </div>
+        <div
+            className={styles.containerModalName}
+            style={{ backgroundColor: color }}
+        >
+            {/* Mostrar en el placeholder el nombre */}
+            <input
+                className={styles.inputName}
+                placeholder="Ingrese nuevo nombre"
+                type="text"
+            />
+            <button className={styles.buttonEdit} onClick={handlerEdit}>
+                <Check color={"#FFFFFF"} width="20" height="20" />
+            </button>
+            <button className={styles.buttonEdit} onClick={handlerEdit}>
+                <Close color={"#FFFFFF"} width="20" height="20" />
+            </button>
         </div>
     );
 };
