2248dedea9fa47e4a3c922de83d3c36a6c969233
SabriBere <sabrinab786@gmail.com>
2025-08-20 23:32:07 -0300

    funcionalida de editar nombre de un posteo

--- src/commons/Modals/ModalEditName.tsx
+++ src/commons/Modals/ModalEditName.tsx
@@ -1,10 +1,15 @@
 "use client";
 import React, { ChangeEvent, useState } from "react";
+import { toast } from "sonner";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateCollection } from "@/services/collection.service";
 import Close from "@/styles/icons/Close";
 import Check from "@/styles/icons/Check";
 import styles from "./modalEditName.module.scss";
+import { updatePost } from "@/services/post.service";
+import { title } from "process";
 
 interface ModalProps {
     id: any;
@@ -17,33 +22,74 @@ interface ModalProps {
 const ModalEditName = ({ id, isOpen, setClose, color }: ModalProps) => {
     const QueryClient = useQueryClient();
     const [newName, setNewName] = useState<string>("");
+    // console.log(newName);
+    const tabs = useSelector((state: RootState) => state.tabs.tabs);
     //captura de valores
-    const body = {
-        title: newName,
-        collectionId: id?.collection_id,
-    };
+    const body: any =
+        tabs === "collections"
+            ? {
+                  title: newName,
+                  collectionId: id?.collection_id,
+              }
+            : {
+                  title: newName,
+                  description: id?.description,
+              };
 
     const handlerInputName = (e: ChangeEvent<HTMLInputElement>) => {
         e.stopPropagation();
         setNewName(e.target.value);
     };
-    //Mutación con react query
-    const { mutateAsync: editNameMutation, isPending: isPendingEdit } =
+    //Mutación con react query para colecciones
+    const {
+        mutateAsync: editCollectionNameMutation,
+        isPending: isPendingEditCollection,
+    } = useMutation({
+        mutationFn: (body: { title: string; collectionId: string | number }) =>
+            updateCollection(body),
+        mutationKey: ["editNameCollection"],
+        onSuccess: async () => {
+            // toast.error("Se muestra el toast?"); //ajustar para cuando sale bien
+            setClose(false);
+            await QueryClient.refetchQueries({
+                queryKey: ["getAllCollections"],
+            });
+        },
+        onError: () => {
+            //llamar al toast con el mensaje de error
+            toast.error("Error al editar nombre");
+            setClose(false);
+        },
+    });
+
+    //Mutación con react query para posteos
+    const { mutateAsync: editPostNameMutation, isPending: isPendingEditPost } =
         useMutation({
-            mutationFn: (body: {
-                title: string;
-                collectionId: string | number;
-            }) => updateCollection(body),
-            mutationKey: ["editNameCollection"],
+            mutationFn: ({
+                body,
+                postId,
+            }: {
+                body: { title?: string; description: string };
+                postId: string | number;
+            }) =>
+                updatePost(
+                    {
+                        title: newName,
+                        description: id?.description,
+                    },
+                    id?.post_id
+                ),
+            mutationKey: ["editNamePost"],
             onSuccess: async () => {
-                //toast con el mensaje de editado correctamente
+                // toast.error("Se muestra el toast?"); //ajustar para cuando sale bien
                 setClose(false);
                 await QueryClient.refetchQueries({
-                    queryKey: ["getAllCollections"],
+                    queryKey: ["getAllPost"],
                 });
             },
             onError: () => {
                 //llamar al toast con el mensaje de error
+                toast.error("Error al editar nombre");
                 setClose(false);
             },
         });
@@ -54,7 +100,10 @@ const ModalEditName = ({ id, isOpen, setClose, color }: ModalProps) => {
 
         if (!newName.trim()) return console.log("Ingrese un nombre"); //mostrar toast
         try {
-            await editNameMutation(body);
+            //según el tab ejeutar una mutación u otra
+            tabs === "collections"
+                ? await editCollectionNameMutation(body)
+                : await editPostNameMutation(body, id?.post_id);
         } catch (error) {
             console.log(error);
         }
@@ -81,14 +130,14 @@ const ModalEditName = ({ id, isOpen, setClose, color }: ModalProps) => {
                 type="text"
                 onChange={handlerInputName}
                 value={newName}
-                disabled={isPendingEdit}
+                disabled={isPendingEditCollection}
             />
             <button
                 type="submit"
                 title="Guardar"
                 className={styles.buttonEdit}
                 onClick={handlerEdit}
-                disabled={isPendingEdit}
+                disabled={isPendingEditCollection}
             >
                 <Check color={"#11796f"} width="20" height="20" />
             </button>
@@ -99,7 +148,7 @@ const ModalEditName = ({ id, isOpen, setClose, color }: ModalProps) => {
                 onClick={(e) => {
                     setClose(false);
                 }}
-                disabled={isPendingEdit}
+                disabled={isPendingEditCollection}
             >
                 <Close color={"#0d1e2b"} width="20" height="20" />
             </button>
