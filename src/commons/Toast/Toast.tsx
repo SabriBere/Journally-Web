b31ee90b1042188d2b89078a7cdb7a37c6a08828
SabriBere <sabrinab786@gmail.com>
2025-08-21 22:51:25 -0300

    dejando toast generico

--- src/commons/Toast/Toast.tsx
+++ src/commons/Toast/Toast.tsx
@@ -8,9 +8,6 @@ const Toast = () => {
         <Toaster
             toastOptions={{
                 style: {
-                    background: "#e74828",
-                    color: "#f5e9d1",
-                    border: "1px solid #e74828",
                     fontFamily: "var(--font-caveat)",
                     fontSize: 18,
                     letterSpacing: "0.6px",
