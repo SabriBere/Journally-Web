a8cc312814282834acf45e1c5d5d847118cb3e06
SabriBere <sabrinab786@gmail.com>
2025-09-21 20:31:39 -0300

    maquetado y funcionalida de breadcrumbs

---
+++ src/commons/Navbar/Breadcrumbs.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { useParams, usePathname, useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { getPostById } from "@/services/post.service";
+import { collectionById } from "@/services/collection.service";
+import styles from "./breadcrumbs.module.scss";
+
+const Breadcrumbs = () => {
+    const router = useRouter();
+    const pathSegment = usePathname();
+    const { id } = useParams();
+    const postId = Number(id);
+    const collectionId = Number(id);
+
+    //detalle de cualquier post
+    const { data: postById } = useQuery({
+        queryKey: ["oneEntry"],
+        queryFn: () => getPostById(postId),
+        enabled: !!postId,
+    });
+
+    //Detalle de una colección
+    const { data: collectionData } = useQuery({
+        queryKey: ["oneCollection"],
+        queryFn: () => collectionById(postId),
+        enabled: !!collectionId,
+    });
+
+    const breadcrumbsList = [
+        {
+            nameOfLastPage: "Entradas",
+            nameOfCurrentPage: `Detalle de entrada - ${postById?.title}`,
+            condition: (pathSegment: string) =>
+                pathSegment === `/entries/${id}`,
+        },
+        {
+            nameOfLastPage: "Colecciones",
+            nameOfCurrentPage: `${collectionData?.collection_name} > ${collectionData?.title}`,
+            condition: (pathSegment: string) =>
+                pathSegment === `/collection/${id}`,
+        },
+    ];
+
+    const getCurrentBreadcrumb = () => {
+        const currentConfig = breadcrumbsList.find((config) =>
+            config.condition(pathSegment)
+        );
+        return currentConfig && <p>{currentConfig.nameOfCurrentPage}</p>;
+    };
+
+    const getLastPageName = (nameOfLastPage: string, index: number) => (
+        <p
+            key={index}
+            className={styles.lastPage}
+            onClick={() => router.back()}
+        >
+            {nameOfLastPage} <span className={styles.space}>{">"}</span>
+        </p>
+    );
+
+    return (
+        <div className={styles.containerBreads}>
+            {breadcrumbsList.map((config, index) =>
+                config.condition(pathSegment)
+                    ? getLastPageName(config.nameOfLastPage, index)
+                    : null
+            )}
+            {getCurrentBreadcrumb()}
+        </div>
+    );
+};
+
+export default Breadcrumbs;
