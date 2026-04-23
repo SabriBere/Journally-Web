8a5b185b763a7d0fe4bdbe24b3745ededa303d1f
Sabrina Demetrio <sdemetrio@globalnewsgroup.com>
2026-04-23 19:52:20 -0300

    navbar responsive

--- src/commons/Buttons/ButtonLogOut.tsx
+++ src/commons/Buttons/ButtonLogOut.tsx
@@ -3,13 +3,20 @@ import React from "react";
 import { signOut } from "next-auth/react";
 import styles from "./buttonLogin.module.scss";
 
-const ButtonLogOut = () => {
+interface ButtonLogOutProps {
+    className?: string;
+}
+
+const ButtonLogOut = ({ className = "" }: ButtonLogOutProps) => {
     const logOut = async () => {
         await signOut({ redirect: true });
     };
 
     return (
-        <button className={styles.buttonLogOut} onClick={logOut}>
+        <button
+            className={[styles.buttonLogOut, className].filter(Boolean).join(" ")}
+            onClick={logOut}
+        >
             <p>Cerrar sesión</p>
         </button>
     );
