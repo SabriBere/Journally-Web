34f84b77f9feea5b84f0f481f8ec51d2a94593b0
Sabrina Demetrio <95036708+SabriBere@users.noreply.github.com>
2026-04-25 21:50:37 -0300

    arreglando estilos del login

--- src/commons/Inputs/InputEmail.tsx
+++ src/commons/Inputs/InputEmail.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/store/store";
 import { setInputEmail } from "@/store/userSlice";
@@ -10,13 +10,9 @@ const InputEmail = () => {
     //Mover al hook de email
     const inputEmail = useSelector((state: RootState) => state.user.email);
     const dispatch = useDispatch();
-    const [isValid, setIsValid] = useState<boolean>(false);
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     const handlerChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value: any = e.target.value;
-        dispatch(setInputEmail(value));
-        setIsValid(emailRegex.test(value));
+        dispatch(setInputEmail(e.target.value));
     };
 
     return (
