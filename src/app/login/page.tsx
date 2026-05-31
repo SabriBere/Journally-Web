aa8a73ed0efbd83fddf1811bc1b194941dcb3238
Sabrina Demetrio <95036708+SabriBere@users.noreply.github.com>
2026-05-30 21:45:27 -0300

    chore: redirect with getSession

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/provider/Credentials";
 import LoginForm from "./components/LoginForm";
 import packageJson from "../../../package.json";
 
-const Login = () => {
+const Login = async () => {
+    const session = await getServerSession(authOptions);
+
+    if (session) {
+        redirect("/home");
+    }
+
     return <LoginForm appVersion={packageJson.version} />;
 };
 
