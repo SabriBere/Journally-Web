5908534e560b395db5edfb5a8a5a78722bf41b4f
Sabrina Demetrio <95036708+SabriBere@users.noreply.github.com>
2026-06-02 15:55:34 -0300

    fix: Duplicated extension names warning

--- src/commons/Editor/editorConfig.ts
+++ src/commons/Editor/editorConfig.ts
@@ -74,35 +74,9 @@ const exitHeadingOnEnter = Extension.create({
     },
 });
 
-const fontSize = Extension.create({
-    name: "fontSize",
-
-    addGlobalAttributes() {
-        return [
-            {
-                types: ["textStyle"],
-                attributes: {
-                    fontSize: {
-                        default: null,
-                        parseHTML: (element) => element.style.fontSize || null,
-                        renderHTML: (attributes) => {
-                            if (!attributes.fontSize) return {};
-
-                            return {
-                                style: `font-size: ${attributes.fontSize}`,
-                            };
-                        },
-                    },
-                },
-            },
-        ];
-    },
-});
-
 export const editorExtensions = [
     StarterKit,
     TextStyleKit,
-    fontSize,
     exitEmptyListItem,
     interpretPastedHtmlText,
     exitHeadingOnEnter,
